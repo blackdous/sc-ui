50d2c3bee0a371a1183622e92aff03122fd0a545
jingguo.zhang <velen@likuan.com>
2023-09-27 10:08:25 +0800

    docs: 更新版本到 0.0.179

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ant-design-vue vue3 ts ui
 
-**最新版本0.0.178**
+**最新版本0.0.179**
 
 [文档地址](http://sc-ui.voneyun.com/)
 
--- src/index.ts
+++ src/index.ts
@@ -142,7 +142,7 @@ const install = (app: App) => {
 };
 
 const ScUi = {
-  version: '0.0.178',
+  version: '0.0.179',
   install,
 };
 
